9137ba34a2e5c69c1a36459245462d28fbcdb825
R4nmaru314 <n.lehmann@r4nmaru.de>
2023-08-27 11:01:12 +0200

    Combine Part1 and Part2

--- advent9.go
+++ advent9.go
@@ -21,6 +21,10 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	coordinatesHead := []Coordinate{{x: 0, y: 0}}
 	coordinatesTail := []Coordinate{{x: 0, y: 0}}
+	coordinatesTails := make([][]Coordinate, 9)
+	for i := 0; i < 9; i++ {
+		coordinatesTails[i] = []Coordinate{{x: 0, y: 0}}
+	}
 	lastCoordinate := Coordinate{x: 0, y: 0}
 	var lastDirection string
 
@@ -29,70 +33,97 @@ func main() {
 		values := strings.Split(line, " ")
 		direction := values[0]
 		count, _ := strconv.Atoi(values[1])
-		calculateCoordinates(&coordinatesHead, &coordinatesTail, &lastCoordinate, direction, count, &lastDirection)
+		calculateCoordinates(&coordinatesHead, &coordinatesTail, coordinatesTails, &lastCoordinate, direction, count, &lastDirection)
 	}
 	lastHeadCoordinate := (coordinatesHead)[len(coordinatesHead)-1]
-	calculateCoordinates(&coordinatesHead, &coordinatesTail, &lastHeadCoordinate, lastDirection, 1, &lastDirection)
+	calculateCoordinates(&coordinatesHead, &coordinatesTail, coordinatesTails, &lastHeadCoordinate, lastDirection, 1, &lastDirection)
 	coordinatesHead = coordinatesHead[:len(coordinatesHead)-1]
 	log.Println(len(removeDuplicates(coordinatesTail)))
-	log.Println(part2())
-	log.Println(part3())
+	log.Println(len(removeDuplicates(coordinatesTails[8])))
 }
 
-func calculateCoordinates(coordinatesHead, coordinatesTail *[]Coordinate, lastCoordinates *Coordinate, direction string, count int, lastDirection *string) {
+func calculateCoordinates(coordinatesHead, coordinatesTail *[]Coordinate, coordinatesTails [][]Coordinate, lastCoordinates *Coordinate, direction string, count int, lastDirection *string) {
 
 	if direction == "R" {
-		calculateCoordinatesRight(coordinatesHead, coordinatesTail, lastCoordinates, count)
+		calculateCoordinatesRight(coordinatesHead, coordinatesTail, coordinatesTails, lastCoordinates, count)
 	} else if direction == "L" {
-		calculateCoordinatesLeft(coordinatesHead, coordinatesTail, lastCoordinates, count)
+		calculateCoordinatesLeft(coordinatesHead, coordinatesTail, coordinatesTails, lastCoordinates, count)
 	} else if direction == "U" {
-		calculateCoordinatesUp(coordinatesHead, coordinatesTail, lastCoordinates, count)
+		calculateCoordinatesUp(coordinatesHead, coordinatesTail, coordinatesTails, lastCoordinates, count)
 	} else if direction == "D" {
-		calculateCoordinatesDown(coordinatesHead, coordinatesTail, lastCoordinates, count)
+		calculateCoordinatesDown(coordinatesHead, coordinatesTail, coordinatesTails, lastCoordinates, count)
 	}
 	*lastDirection = direction
 }
 
-func calculateCoordinatesRight(coordinatesHead, coordinatesTail *[]Coordinate, lastCoordinate *Coordinate, count int) {
+func calculateCoordinatesRight(coordinatesHead, coordinatesTail *[]Coordinate, coordinatesTails [][]Coordinate, lastCoordinate *Coordinate, count int) {
 	for i := 0; i < count; i++ {
 		newCoordinate := Coordinate{lastCoordinate.x + 1, lastCoordinate.y}
 		*coordinatesHead = append(*coordinatesHead, newCoordinate)
 		calculateTail(coordinatesTail, lastCoordinate)
+		calculateTails(coordinatesTails, lastCoordinate)
 		*lastCoordinate = newCoordinate
 	}
 }
 
-func calculateCoordinatesLeft(coordinatesHead, coordinatesTail *[]Coordinate, lastCoordinate *Coordinate, count int) {
+func calculateCoordinatesLeft(coordinatesHead, coordinatesTail *[]Coordinate, coordinatesTails [][]Coordinate, lastCoordinate *Coordinate, count int) {
 	for i := 0; i < count; i++ {
 		newCoordinate := Coordinate{lastCoordinate.x - 1, lastCoordinate.y}
 		*coordinatesHead = append(*coordinatesHead, newCoordinate)
 		calculateTail(coordinatesTail, lastCoordinate)
+		calculateTails(coordinatesTails, lastCoordinate)
 		*lastCoordinate = newCoordinate
 	}
 }
 
-func calculateCoordinatesUp(coordinatesHead, coordinatesTail *[]Coordinate, lastCoordinate *Coordinate, count int) {
+func calculateCoordinatesUp(coordinatesHead, coordinatesTail *[]Coordinate, coordinatesTails [][]Coordinate, lastCoordinate *Coordinate, count int) {
 	for i := 0; i < count; i++ {
 		newCoordinate := Coordinate{lastCoordinate.x, lastCoordinate.y + 1}
 		*coordinatesHead = append(*coordinatesHead, newCoordinate)
 		calculateTail(coordinatesTail, lastCoordinate)
+		calculateTails(coordinatesTails, lastCoordinate)
 		*lastCoordinate = newCoordinate
 	}
 }
 
-func calculateCoordinatesDown(coordinatesHead, coordinatesTail *[]Coordinate, lastCoordinate *Coordinate, count int) {
+func calculateCoordinatesDown(coordinatesHead, coordinatesTail *[]Coordinate, coordinatesTails [][]Coordinate, lastCoordinate *Coordinate, count int) {
 	for i := 0; i < count; i++ {
 		newCoordinate := Coordinate{lastCoordinate.x, lastCoordinate.y - 1}
 		*coordinatesHead = append(*coordinatesHead, newCoordinate)
 		calculateTail(coordinatesTail, lastCoordinate)
+		calculateTails(coordinatesTails, lastCoordinate)
 		*lastCoordinate = newCoordinate
+	}
+}
+
+func calculateTails(coordinatesTails [][]Coordinate, lastCoordinate *Coordinate) {
+	calculateTail(&coordinatesTails[0], lastCoordinate)
+	*lastCoordinate = (coordinatesTails[0])[len(coordinatesTails[0])-1]
+	for i := 1; i < 9; i++ {
+		calculateTail(&coordinatesTails[i], lastCoordinate)
+		*lastCoordinate = (coordinatesTails[i])[len(coordinatesTails[i])-1]
 	}
 }
 
 func calculateTail(coordinatesTail *[]Coordinate, lastCoordinate *Coordinate) {
 	lastTailCoordinate := (*coordinatesTail)[len(*coordinatesTail)-1]
 
-	if isTwoUnitsAwayX(*lastCoordinate, lastTailCoordinate) {
+	if isTwoUnitsAwayY(*lastCoordinate, lastTailCoordinate) && isTwoUnitsAwayX(*lastCoordinate, lastTailCoordinate) {
+		if lastTailCoordinate.y < lastCoordinate.y {
+			if lastTailCoordinate.x < lastCoordinate.x {
+				*coordinatesTail = append(*coordinatesTail, Coordinate{lastTailCoordinate.x + 1, lastTailCoordinate.y + 1})
+
+			} else {
+				*coordinatesTail = append(*coordinatesTail, Coordinate{lastTailCoordinate.x - 1, lastTailCoordinate.y + 1})
+			}
+		} else {
+			if lastTailCoordinate.x < lastCoordinate.x {
+				*coordinatesTail = append(*coordinatesTail, Coordinate{lastTailCoordinate.x + 1, lastTailCoordinate.y - 1})
+			} else {
+				*coordinatesTail = append(*coordinatesTail, Coordinate{lastTailCoordinate.x - 1, lastTailCoordinate.y - 1})
+			}
+		}
+	} else if isTwoUnitsAwayX(*lastCoordinate, lastTailCoordinate) {
 		if lastTailCoordinate.x < lastCoordinate.x {
 			*coordinatesTail = append(*coordinatesTail, Coordinate{lastTailCoordinate.x + 1, lastCoordinate.y})
 		} else {
